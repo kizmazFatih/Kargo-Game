275fdacdfe6a4986658f962e9c2cec80e4594a39
chimpsz <156349027+chimpsz@users.noreply.github.com>
2024-10-31 00:08:08 +0300

    Reapply "Car Mechanic"
    This reverts commit 896ccec20084383278d8d4a857f8f988840e7f57.

---
+++ Assets/Scripts/CarInputs.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class CarInputs : MonoBehaviour
+{
+
+
+    [HideInInspector] public float gas_input;
+    [HideInInspector] public float turn_input;
+
+
+    public void CarMove(InputAction.CallbackContext context)
+    {
+
+        gas_input = context.ReadValue<Vector2>().y;
+        turn_input = context.ReadValue<Vector2>().x;
+
+
+    }
+}
---
+++ Assets/Scripts/Steering.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+    [SerializeField] private CarInputs carInputs;
+    [SerializeField] private CarController carController;
+    private Suspension suspension;
+    private Transform tire;
+    private Rigidbody rb;
+
+    [Header("Steering")]
+    [SerializeField] private float tire_grip_factor;
+    [SerializeField] private bool turnable;
+    private float turn_input;
+
+
+    void Start()
+    {
+        carController = transform.root.GetComponent<CarController>();
+        suspension = GetComponent<Suspension>();
+
+        tire = suspension.tire;
+        rb = suspension.rb;
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (carController.work)
+        {
+
+            if (turnable)
+            {
+                TurnToTire();
+            }
+
+
+
+            if (suspension.rayDidHit)
+            {
+
+                Vector3 steering_direction = tire.right;
+                Vector3 tire_world_velocity = rb.GetPointVelocity(tire.position);
+
+                float steering_velocity = Vector3.Dot(steering_direction, tire_world_velocity);
+                float desired_velocity_change = -steering_velocity * tire_grip_factor;
+
+                float desired_acceleration = desired_velocity_change / Time.fixedDeltaTime;
+
+
+                rb.AddForceAtPosition(desired_acceleration * steering_direction * 50*Time.fixedDeltaTime, tire.position);
+
+
+            }
+        }
+
+
+
+
+
+    }
+
+    void TurnToTire()
+    {
+
+        turn_input = carInputs.turn_input;
+        float tire_angle = turn_input * 25;
+        tire.localRotation = Quaternion.Euler(0, tire_angle, tire.localEulerAngles.z);
+    }
+}
