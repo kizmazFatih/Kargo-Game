de3f881d2a01f8690932715e87bd2f06b12602da
chimpsz <156349027+chimpsz@users.noreply.github.com>
2024-11-02 23:41:24 +0300

    CarrySystem

---
+++ Assets/Scripts/CarrySystem/CarrySystem.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic; // Liste kullanabilmek için
+using UnityEngine;
+
+public class CarrySystem : MonoBehaviour
+{
+    public Transform targetPosition; // Kutu konumunun alınacağı boş obje
+    private List<GameObject> carriedBoxes = new List<GameObject>(); // Taşınan kutuların listesi
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Box")) // Kutuya temas
+        {
+            // Eğer bu kutu daha önce alınmamışsa
+            if (!carriedBoxes.Contains(other.gameObject))
+            {
+                carriedBoxes.Add(other.gameObject); // Kutuyu listeye ekle
+                PositionBoxes(); // Kutuların pozisyonlarını ayarla
+            }
+        }
+    }
+
+   
+
+    private void PositionBoxes()
+    {
+
+        // İlk kutuyu hedef pozisyona yerleştir
+        GameObject currentBox = carriedBoxes[carriedBoxes.Count - 1];
+        currentBox.transform.SetParent(targetPosition); // İlk kutuyu Target objesine child yap
+        currentBox.transform.localPosition = Vector3.zero + new Vector3(0, carriedBoxes.Count - 1, 0); // İlk kutuyu hedefin konumuna yerleştir
+        currentBox.transform.rotation = targetPosition.rotation; // İlk kutunun açısını ayarla
+
+
+
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+    [SerializeField] private float speed;
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float _velocity;
+
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    private void ApplyGravity()
+    {
+        if (_characterController.isGrounded && _velocity < 0.0f )
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        } 
+        _direction.y = _velocity;
+    }
+
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0) return;
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+    }
+}
